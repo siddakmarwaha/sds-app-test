6dd5c1b84f037a889ebd1ce59f39cb68607cb37b
Siddak Singh Marwaha <112235636+siddakmarwaha@users.noreply.github.com>
2026-02-13 04:07:12 -0800

    Fix formatting in input_birthdata print statement

--- main.py
+++ main.py
@@ -54,7 +54,8 @@ def generate_chart(data: BirthData):
 
         print(f"input_birthdata called with: name={data.name}, gender={data.gender}, "
           f"year={year}, month={str(int(month))}, day={str(int(day))}, hour={str(int(hour))}, min={str(int(minute))}, "
-          f"lon={str(data.longitude)}, lat={str(data.latitude)}, tz={f"+{data.timezone}" if data.timezone >= 0 else f"-{data.timezone}}")
+          f"lon={str(data.longitude)}, lat={str(data.latitude)}, tz={f"+{data.timezone}" if data.timezone >= 0 else f"-{data.timezone}"}")
+          
 
         birthdata = jyotishyamitra.get_birthdata()
         print(f"birthdata result: {birthdata}")
